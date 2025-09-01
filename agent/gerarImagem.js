425e9b1589130082826715d1d6274df6542efc31
dedicatorias-web <arpt1971@gmail.com>
2025-09-01 13:44:58 -0300

    Update gerarImagem.js

--- agent/gerarImagem.js
+++ agent/gerarImagem.js
@@ -2,17 +2,20 @@ const fs = require("fs");
 const path = require("path");
 const https = require("https");
 
-async function baixarImagem(url, nomeArquivo) {
+async function gerarImagem(roteiro, nomeArquivo) {
+  // Simula geração de imagem com IA usando uma imagem de exemplo
+  const urlImagem = "https://copilot.microsoft.com/th/id/BCO.73022c02-25b3-413f-86f8-9649db93675b.png";
   const destino = path.join(__dirname, "..", "docs", "imagens", nomeArquivo);
+
   return new Promise((resolve, reject) => {
-    https.get(url, response => {
+    https.get(urlImagem, response => {
       const file = fs.createWriteStream(destino);
       response.pipe(file);
       file.on("finish", () => {
-        file.close(resolve);
+        file.close(() => resolve(nomeArquivo));
       });
     }).on("error", reject);
   });
 }
 
-module.exports = { baixarImagem };
+module.exports = { gerarImagem };
