3d81e47f19ac4ef021504565334ec4e9a85d6f7f
dedicatorias-web <arpt1971@gmail.com>
2025-09-01 19:37:23 -0300

    Update salvarHQ.js

--- agent/salvarHQ.js
+++ agent/salvarHQ.js
@@ -18,7 +18,10 @@ function salvarHQ(noticia, roteiro, nomeImagem) {
     roteiro
   };
 
+  // Salva HQ atual
   fs.writeFileSync(path.join(__dirname, "..", "docs", "data.json"), JSON.stringify(hq, null, 2));
+
+  // Salva histórico
   fs.writeFileSync(path.join(__dirname, "..", "docs", "hqs", `${hoje}.json`), JSON.stringify(hqCompleta, null, 2));
 }
 
